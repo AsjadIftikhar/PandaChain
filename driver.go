3b0356172d8f22ed616d56e367b08cf96040e84b
kai <iftikharasjad@gmail.com>
2022-03-08 02:19:27 +0500

    completed

--- driver.go
+++ driver.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 )
 
@@ -13,7 +15,16 @@ type Block struct {
 
 func CalculateHash(inputBlock *Block) string {
 	//Calculate Hash of a Block
-	return ""
+	message := ""
+	for i := 0; i < len(inputBlock.transactions); i++ {
+		message += inputBlock.transactions[i]
+	}
+	message += inputBlock.prevHash
+	converted := []byte(message)
+	hashTool := sha256.New()
+	hashTool.Write(converted)
+
+	return hex.EncodeToString(hashTool.Sum(nil))
 }
 
 func InsertBlock(transactionsToInsert []string, chainHead *Block) *Block {
@@ -26,21 +37,39 @@ func InsertBlock(transactionsToInsert []string, chainHead *Block) *Block {
 		currentHash:  "",
 	}
 
+	if chainHead != nil {
+		newBlock.prevHash = chainHead.currentHash
+	}
+
+	newBlock.currentHash = CalculateHash(&newBlock)
+
 	return &newBlock
 }
 
 func ChangeBlock(oldTrans string, newTrans string, chainHead *Block) {
 
 	//change transaction data inside block
+	currPointer := chainHead
+	for i := 0; currPointer != nil; i++ {
+		for j := 0; j < len(currPointer.transactions); j++ {
+			if currPointer.transactions[j] == oldTrans {
+				currPointer.transactions[j] = newTrans
+			}
+		}
+
+		currPointer = currPointer.prevPointer
+	}
+
 }
 
 func ListBlocks(chainHead *Block) {
 
 	//display the data(transaction) inside all blocks
+	fmt.Println()
 	currPointer := chainHead
 	for i := 0; currPointer != nil; i++ {
-		fmt.Print(currPointer.transactions)
-		fmt.Print("->")
+		fmt.Print(currPointer.transactions, " | ", currPointer.currentHash, " | ", currPointer.prevHash)
+		fmt.Println("<--")
 
 		currPointer = currPointer.prevPointer
 	}
@@ -50,22 +79,31 @@ func ListBlocks(chainHead *Block) {
 func VerifyChain(chainHead *Block) {
 
 	//check whether "Block chain is compromised" or "Block chain is unchanged"
+	currPointer := chainHead.prevPointer
+	prevHash := chainHead.prevHash
+
+	for i := 0; currPointer != nil; i++ {
+		currHash := CalculateHash(currPointer)
+		if currHash != prevHash {
+			fmt.Println("Chain Has Been Compromised")
+			return
+		}
+		prevHash = currPointer.prevHash
+		currPointer = currPointer.prevPointer
+	}
+	fmt.Println("Chain is unchanged")
 
 }
 func main() {
 
-	transactions := []string{"Alice Sent $100 to Bob", "Bob Received $50"}
-	transactions1 := []string{"A"}
-	transactions2 := []string{"B"}
-	transactions3 := []string{"C"}
-	transactions4 := []string{"D"}
+	chainHead := InsertBlock([]string{"Alice Sent $100 to Bob", "Bob Sent $50 to Chad"}, nil)
+	chainHead = InsertBlock([]string{"Alice Minted an NFT"}, chainHead)
+	chainHead = InsertBlock([]string{"Bob is Alive"}, chainHead)
+	chainHead = InsertBlock([]string{"Why am I Doing this?", "Such a bad assignment tbh!", "Lets finish it ASAP"}, chainHead)
+	chainHead = InsertBlock([]string{"Stupid Course"}, chainHead)
 
-	chainHead := InsertBlock(transactions, nil)
-	chainHead = InsertBlock(transactions1, chainHead)
-	chainHead = InsertBlock(transactions2, chainHead)
-	chainHead = InsertBlock(transactions3, chainHead)
-	chainHead = InsertBlock(transactions4, chainHead)
-
+	//ChangeBlock("Bob is Alive", "Bob is dead", chainHead)
 	ListBlocks(chainHead)
+	VerifyChain(chainHead)
 
 }
